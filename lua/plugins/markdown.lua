84c62c420ada717b7d2f99ac34a86a94fd8c2bb9
pratapgriphic <bhanuprataps@griphic.com>
2026-05-04 15:11:18 +0530

    feat: add markdown plugin

---
+++ lua/plugins/markdown.lua
@@ -0,0 +1,8 @@
+return {
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    build = "cd app && npm install",
+    ft = { "markdown" },
+  },
+}
