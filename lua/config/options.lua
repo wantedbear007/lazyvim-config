447a2662592a690a52b9e2588bce97d6801afb76
Bhanupratap Singh <pratapsinghbhanu444@gmail.com>
2026-05-22 15:48:13 +0530

    feat: add rust config

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,7 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 --
-
+vim.g.lazyvim_rust_diagnostics = "rust-analyzer"
 -- vim.cmd.colorscheme("onedark")
 vim.opt.number = true
 vim.opt.termguicolors = true
---
+++ lua/plugins/toogleterm.lua
@@ -0,0 +1,44 @@
+return {
+  {
+    "akinsho/toggleterm.nvim",
+    version = "*",
+    config = function()
+      require("toggleterm").setup({
+        size = 20,
+        open_mapping = [[<c-\>]],
+        hide_numbers = true,
+        shade_terminals = true,
+        start_in_insert = true,
+        insert_mappings = true,
+        terminal_mappings = true,
+        persist_size = true,
+        direction = "float",
+
+        float_opts = {
+          border = "curved",
+        },
+      })
+
+      -- terminal keymaps
+      function _G.set_terminal_keymaps()
+        local opts = { buffer = 0 }
+
+        vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
+        vim.keymap.set("t", "jk", [[<C-\><C-n>]], opts)
+
+        vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
+        vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
+        vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
+        vim.keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts)
+      end
+
+      vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
+    end,
+
+    keys = {
+      { "<leader>tf", "<cmd>ToggleTerm direction=float<cr>", desc = "Float Terminal" },
+      { "<leader>th", "<cmd>ToggleTerm size=15 direction=horizontal<cr>", desc = "Horizontal Terminal" },
+      { "<leader>tv", "<cmd>ToggleTerm size=60 direction=vertical<cr>", desc = "Vertical Terminal" },
+    },
+  },
+}
